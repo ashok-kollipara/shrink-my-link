b97615f5687a51161a48b0fcb1d726ba77867b56
ashok-kollipara <93081449+ashok-kollipara@users.noreply.github.com>
2022-08-15 19:54:47 +0000

    add arguments functionality

--- shrink.py
+++ shrink.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import asyncio
 import aiohttp
 import json
@@ -7,9 +8,10 @@ from src import constants as constants
 
 class Shorten: 
 
-    def __init__(self, long_url, custom_alias):
+    def __init__(self, long_url, custom_alias, verbosity):
         self.long_url = long_url
         self.custom_alias = custom_alias
+        self.verbosity = verbosity
 
     async def tinyurl(self):
 
@@ -31,14 +33,18 @@ class Shorten:
 
             async with session.post(url, data=payload) as reply:
 
-                #print(f"TinyURL Status : {reply.status} - {reply.reason}")
+                if self.verbosity:
+                    print(f"TinyURL Status : {reply.status} - {reply.reason}")
 
                 if reply.ok:
                     #convert the text from response received to python dict
                     txt = await reply.text()
                     result = json.loads(txt)
                     #print(f"type of result {type(result)}")
-                    print(f"TinyUrl : {result['data']['tiny_url']}")
+                    if self.verbosity:
+                        print(f"TinyUrl : {result['data']['tiny_url']}")
+                    else:
+                        print(result['data']['tiny_url'])
 
     async def cuttly(self):
 
@@ -52,26 +58,35 @@ class Shorten:
 
             async with session.get(url) as reply:
 
-                #print(f"Cuttly Status : {reply.status} - {reply.reason}")
+                if self.verbosity:
+                    print(f"Cuttly Status : {reply.status} - {reply.reason}")
 
                 if reply.ok:
                     #convert the text from response received to python dict
                     txt = await reply.text()
                     result = json.loads(txt)
-                    print(f"Cuttly : {result['url']['shortLink']}")
 
-async def main():
+                    if self.verbosity:
+                        print(f"Cuttly : {result['url']['shortLink']}")
+                    else:
+                        print(result['url']['shortLink'])
 
-    long_url = input('Enter your URL : ')
-    custom_alias = input('Enter your alias for URL [5 - 20] chars long: ')
+async def main(long_url, custom_alias, verbosity, quickmode):
+
+    '''
+    print(long_url)
+    print(custom_alias)
+    print(verbosity)
+    '''
 
     print()
 
-    test = Shorten(long_url, custom_alias)
+    test = Shorten(long_url, custom_alias, verbosity)
 
     tasks = [ asyncio.create_task(test.cuttly()) ]
     
-    tasks.append( asyncio.create_task(test.tinyurl()) )
+    if quickmode:
+        tasks.append( asyncio.create_task(test.tinyurl()) )
 
     await asyncio.gather(*tasks)
 
@@ -79,4 +94,26 @@ async def main():
 
 if __name__ == '__main__':
 
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(prog="shrink", description="To shorten the given Long URL")
+
+    # traditional arguments
+
+    parser.add_argument("URL", type=str, help="the long url to shorten")
+    parser.add_argument("-v", "--verbose", action="store_true", help="add verbosity to output")
+
+    # below two arguments can be changed to mutually exclusive group
+    # user can modify as per usage scope viz; parser.add_mutually_exclusive_group()
+
+    parser.add_argument("-a", "--alias", action="store", default="", help="try for custom back-half e.g bit.ly/{ALIAS}")
+    parser.add_argument("-q", "--quick", action="store_false", help="quickly get single link, useful to chain commands in Linux")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+
+        print (f"URL to be shortened : {args.URL}")
+
+        if args.alias:
+            print(f"alias chosen is : {args.alias}")
+
+    asyncio.run(main(args.URL, args.alias, args.verbose, args.quick))
